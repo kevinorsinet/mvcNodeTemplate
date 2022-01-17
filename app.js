d4838ef413efaa78c8fd14116dd1a5d7b348861e
kevinorsinet <kevin.orsinet@gmail.com>
2022-01-17 10:53:32 +0100

    Variables EJS

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const app = express()
 // Config moteur de templating
 app.set('view engine', 'ejs');
 
+
 // Controller
 app.get('/', function (req, res) {
-  res.render('index')
+  // Data
+  const user = {
+    name: "Kévin",
+    age: 27
+  }
+  console.log(user.name);
+  // res.render('index', user)
+  // res.render('index', {user: user})
+  // res.render('index', { toto: user})
+  res.render('index', {user})
 })
 
 app.get('/contact', function(req,res){
